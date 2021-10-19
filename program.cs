a508d0f9b5ab89a5927e9b5493ed2166ee8e15a4
LeKazo <70152390+LeKazo@users.noreply.github.com>
2021-10-19 09:40:48 -0400

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        
+            static void Main()
+            {
+                Console.WriteLine("How old are you?");
+                string input = Console.ReadLine();
+                Console.WriteLine($"You are {input} years old!");
+            }
+    }
+}
+    
